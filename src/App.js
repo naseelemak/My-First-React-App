2b02756a236aaf0cbe6985733a2faddf23ba146b
naseelemak <taseelemak@gmail.com>
2020-12-02 11:06:53 +0800

    Add routing
    Using the react-router-dom library

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import HelloWorld from "./Components/HelloWorld";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header />
+    <Router>
+      <div>
+        <Header />
 
-      <HelloWorld name="Jaedon" />
+        <HelloWorld name="Jaedon" />
 
-      <Footer />
-    </div>
+        <ul>
+          <li>
+            <Link to="/" className="text-blue-500">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="text-blue-500">
+              About
+            </Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/">
+            <h1 className="font-bold text-2xl">Welcome home</h1>
+          </Route>
+
+          <Route path="/about">
+            <h1 className="font-bold text-2xl">About</h1>
+          </Route>
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
