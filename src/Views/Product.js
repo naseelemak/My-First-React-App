0a14fe4d028f893eb72e78e975080e91e0072fa7
naseelemak <taseelemak@gmail.com>
2020-12-04 11:14:52 +0800

    Add loading symbol and axion.catch

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,30 +1,64 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import { useParams } from "react-router-dom";
+import Loader from "../Components/Loader";
 
 function Product() {
-  const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState({
+    loading: false,
+    data: null,
+    error: false,
+  });
 
   const { id } = useParams();
   const url = `https://5fc8b0fe2af77700165adbc4.mockapi.io/api/v1/products/${id}`;
 
   let content = null;
 
+  // loads data from the API and throws an error message if it fails
   useEffect(() => {
-    Axios.get(url).then((response) => {
-      setProduct(response.data);
-    });
+    setProduct({ loading: true, data: null, error: false });
+
+    Axios.get(url)
+      .then((response) => {
+        setProduct({
+          loading: false,
+          data: response.data,
+          error: false,
+        });
+      })
+      .catch(() => {
+        setProduct({
+          loading: false,
+          data: null,
+          error: true,
+        });
+      });
   }, [url]);
 
-  if (product) {
+  // display error message if data retrieval fails
+  if (product.error) {
+    content = (
+      <p className="flex justify-center mt-3">
+        There was an error. Please refresh page or try again later.
+      </p>
+    );
+  }
+
+  // display loading icon when data form API is still being retrieved
+  if (product.loading) {
+    content = <Loader />;
+  }
+
+  if (product.data) {
     content = (
       <div>
-        <h1 className="text-2xl font-bold mb-3">{product.name}</h1>
+        <h1 className="text-2xl font-bold mb-3">{product.data.name}</h1>
         <div>
-          <img src={product.images[0].imageUrl} alt={product.name} />
+          <img src={product.data.images[0].imageUrl} alt={product.data.name} />
         </div>
-        <div className="text-xl font-bold mb-3">$ {product.price}</div>
-        <div>{product.description}</div>
+        <div className="text-xl font-bold mb-3">$ {product.data.price}</div>
+        <div>{product.data.description}</div>
       </div>
     );
   }
